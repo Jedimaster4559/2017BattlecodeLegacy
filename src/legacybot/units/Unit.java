ffe6a1b642004ca402db43de8befca677a71c8b7
Jedimaster4559 <nbs0007@uah.edu>
2019-12-26 03:11:38 -0600

    added start and end round methods in Unit and refactored all units to behave with this in mind

--- src/legacybot/units/Unit.java
+++ src/legacybot/units/Unit.java
@@ -4,6 +4,9 @@ import battlecode.common.*;
 
 import java.util.Random;
 
+import static legacybot.tools.UnsortedTools.checkVictory;
+import static legacybot.tools.UnsortedTools.tryShake;
+
 public abstract class Unit {
 
     // This is the RobotController object. You use it to perform actions from this robot,
@@ -11,6 +14,7 @@ public abstract class Unit {
     RobotController rc;
     Team enemy;
     Random rand;
+    int age; // The number of rounds this unit has been alive.
 
     public Unit(RobotController rc){
         this.rc = rc;
@@ -18,7 +22,31 @@ public abstract class Unit {
         
         //rand = new Random(1000);
         rand = new Random();
+
+        age = 0;
     }
 
     public abstract void run() throws GameActionException;
+
+    public final void roundStart() throws GameActionException {
+        // Try to shake any trees that we can
+        tryShake(rc);
+
+        // Check to see if we can win the match
+        checkVictory(rc);
+    }
+
+    public final void roundEnd() throws GameActionException {
+        // Try to shake any trees that we can
+        tryShake(rc);
+
+        // Check to see if we can win the match
+        checkVictory(rc);
+
+        // Increment the age counter
+        age++;
+
+        // Clock.yield() makes the robot wait until the next turn, then it will perform this loop again
+        Clock.yield();
+    }
 }
