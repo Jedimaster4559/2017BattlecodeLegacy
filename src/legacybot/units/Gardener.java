7b9f93bf016e0cf630867f4c13b1fe990fc5556c
Jedimaster4559 <nbs0007@uah.edu>
2019-12-26 04:04:05 -0600

    added first part of logic for finding where to plant things

--- src/legacybot/units/Gardener.java
+++ src/legacybot/units/Gardener.java
@@ -4,9 +4,17 @@ import battlecode.common.*;
 import static legacybot.tools.UnsortedTools.*;
 
 public class Gardener extends Unit{
+    GardenerUnitState state;
+    boolean hasBuiltInitialLumberjack;
+    int roundsSinceLastBuild;
+    int totalUnitsBuilt;
 
     public Gardener(RobotController rc){
         super(rc);
+        state = GardenerUnitState.INITIAL;
+        hasBuiltInitialLumberjack = false;
+        roundsSinceLastBuild = Integer.MAX_VALUE;
+        totalUnitsBuilt = 0;
     }
 
     public void run() throws GameActionException {
@@ -21,23 +29,22 @@ public class Gardener extends Unit{
                 //Start the round
                 roundStart();
 
-                // Listen for home archon's location
-                int xPos = rc.readBroadcast(0);
-                int yPos = rc.readBroadcast(1);
-                MapLocation archonLoc = new MapLocation(xPos,yPos);
+                checkState();
 
-                // Generate a random direction
-                Direction dir = randomDirection();
-
-                // Randomly attempt to build a soldier or lumberjack in this direction
-                if (rc.canBuildRobot(RobotType.SOLDIER, dir) && Math.random() < .01) {
-                    rc.buildRobot(RobotType.SOLDIER, dir);
-                } else if (rc.canBuildRobot(RobotType.LUMBERJACK, dir) && Math.random() < .01 && rc.isBuildReady()) {
-                    rc.buildRobot(RobotType.LUMBERJACK, dir);
+                switch(state){
+                    case FARMING:
+                        farming();
+                        break;
+                    case SEARCHING:
+                        searching();
+                        break;
+                    case INITIAL:
+                        initial();
+                        break;
                 }
 
-                // Move randomly
-                tryMove(randomDirection(), rc);
+                // Increment rounds since last build
+                roundsSinceLastBuild++;
 
                 // End the round
                 roundEnd();
@@ -47,5 +54,99 @@ public class Gardener extends Unit{
                 e.printStackTrace();
             }
         }
+    }
+
+    public void checkState() {
+        if(state == GardenerUnitState.FARMING){
+            return;
+        }
+
+        if(age > 10 && hasBuiltInitialLumberjack){
+            state = GardenerUnitState.SEARCHING;
+        }
+    }
+
+    public void initial() throws GameActionException{
+        if(!hasBuiltInitialLumberjack){
+            if(tryBuild(RobotType.LUMBERJACK)){
+                hasBuiltInitialLumberjack = true;
+            }
+        }
+
+        if(rc.getLocation().distanceTo(spawn) < 0.01){
+            tryMove(randomDirection(), rc);
+        } else {
+            // Continue moving away from our spawn along previous vector
+            Direction direction = spawn.directionTo(rc.getLocation());
+            tryMove(direction, rc);
+        }
+    }
+
+    public void searching() throws GameActionException {
+        // attempt to build
+        if(roundsSinceLastBuild > 250 || rand.nextFloat() < .05){
+            if(rand.nextBoolean()){
+                tryBuild(RobotType.LUMBERJACK);
+            } else {
+                tryBuild(RobotType.SOLDIER);
+            }
+        }
+
+        //Check if this is a farmable location
+        RobotInfo[] robots = rc.senseNearbyRobots(-1, enemy.opponent());
+
+        MapLocation plantObstacle = null; // Basicially, anything we don't want to plant near
+        for(RobotInfo robot : robots){
+            if(robot.type == RobotType.ARCHON){
+                plantObstacle = robot.getLocation();
+                break;
+            }
+
+            if(robot.type == RobotType.GARDENER && rc.getLocation().distanceTo(robot.getLocation()) < 2 * (RobotType.GARDENER.bodyRadius + GameConstants.BULLET_TREE_RADIUS * 2)){
+                plantObstacle = robot.getLocation();
+            }
+        }
+
+        if(plantObstacle == null){
+            state = GardenerUnitState.FARMING;
+            return;
+        } else {
+            tryMove(plantObstacle.directionTo(rc.getLocation()), rc);
+        }
+    }
+
+    public void farming(){
+
+    }
+
+    /**
+     * Helper to assist with the production of units
+     * @param type The type of robot to create
+     * @return If a unit was actually created or not
+     * @throws GameActionException
+     */
+    private boolean tryBuild(RobotType type) throws GameActionException{
+        Direction buildDirection = randomDirection();
+
+        // TODO: This can be optimized a lot
+        // Try rotating instead of random
+        // Allow some type of escape cause this sometimes might break
+        if(rc.getTeamBullets() >= type.bulletCost) {
+            while (!rc.canBuildRobot(type, buildDirection)) {
+                buildDirection = randomDirection();
+                if(Clock.getBytecodesLeft() < 5000){
+                    break;
+                }
+            }
+        }
+
+        if(rc.canBuildRobot(type, buildDirection)){
+            rc.buildRobot(type, buildDirection);
+            roundsSinceLastBuild = 0;
+            totalUnitsBuilt++;
+            return true;
+        }
+
+        return false;
     }
 }
