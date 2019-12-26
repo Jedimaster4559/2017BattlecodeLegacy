3056937d9b75f71c82f019fb5ea5623e28a3a141
Jedimaster4559 <nbs0007@uah.edu>
2019-12-26 02:02:39 -0600

    Added basic state machine for the archon to use

--- src/legacybot/units/Archon.java
+++ src/legacybot/units/Archon.java
@@ -5,8 +5,17 @@ import static legacybot.tools.UnsortedTools.*;
 
 public class Archon extends Unit{
 
+    ArchonUnitState state;
+    int roundsSinceLastGardner;
+    int gardenersBuilt;
+    int totalArchonNum;
+
     public Archon(RobotController rc){
         super(rc);
+        state = ArchonUnitState.INITIAL;
+        roundsSinceLastGardner = Integer.MAX_VALUE;
+        gardenersBuilt = 0;
+        totalArchonNum = rc.getInitialArchonLocations(enemy).length;
     }
 
     public void run() throws GameActionException {
@@ -21,24 +30,32 @@ public class Archon extends Unit{
                 // Try to shake any trees that we can
                 tryShake(rc);
 
-                // Generate a random direction
-                Direction dir = randomDirection();
+                checkVictory(rc);
 
-                // Randomly attempt to build a gardener in this direction
-                if (rc.canHireGardener(dir) && Math.random() < .01) {
-                    rc.hireGardener(dir);
+                checkState();
+
+                switch (state){
+                    case SLOW:
+                        slow();
+                        break;
+                    case FAST:
+                        fast();
+                        break;
+                    case AGGRESSIVE:
+                        aggressive();
+                        break;
+                    case INITIAL:
+                        initial();
+                        break;
+                    case DEFENSIVE:
+                        defensive();
+                        break;
                 }
-
-                // Move randomly
-                tryMove(randomDirection(), rc);
-
-                // Broadcast archon's location for other robots on the team to know
-                //MapLocation myLocation = rc.getLocation();
-                //.broadcast(0,(int)myLocation.x);
-                //.broadcast(1,(int)myLocation.y);
 
                 // Try to shake any trees that we can
                 tryShake(rc);
+
+                checkVictory(rc);
 
                 // Clock.yield() makes the robot wait until the next turn, then it will perform this loop again
                 Clock.yield();
@@ -48,5 +65,54 @@ public class Archon extends Unit{
                 e.printStackTrace();
             }
         }
+    }
+
+    public void checkState() throws GameActionException {
+        int round = rc.getRoundNum();
+
+        if(round < 10){
+            state = ArchonUnitState.INITIAL;
+            return;
+        }
+
+        if (round > 2750){
+            state = ArchonUnitState.DEFENSIVE;
+            return;
+        }
+
+        if(round > 1000 && round / 100 > gardenersBuilt){
+            state = ArchonUnitState.SLOW;
+            return;
+        } else if (round / 100 / totalArchonNum > gardenersBuilt){
+            state = ArchonUnitState.FAST;
+            return;
+        }
+
+        if(round > 2000 || rc.readBroadcast(13) != 0){
+            state = ArchonUnitState.AGGRESSIVE;
+            return;
+        }
+
+        state = ArchonUnitState.FAST;
+    }
+
+    public void slow(){
+
+    }
+
+    public void initial(){
+
+    }
+
+    public void aggressive(){
+
+    }
+
+    public void fast(){
+
+    }
+
+    public void defensive(){
+
     }
 }
